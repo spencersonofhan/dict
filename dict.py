69341b6bc17b53804ca215bf5649f2fdf0f9341b
Spencer Hanson <spencer.hanson@aggiemail.usu.edu>
2020-04-27 21:21:31 -0600

    Got initial structure working. It recieves a JSON back successfuly; however, I need to find out what and how to extract the desired data.

--- dict.py
+++ dict.py
@@ -1,3 +1,15 @@
 import requests
+import sys
 
-print("hello")
+apiRequest = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
+
+searchWord = sys.argv[1]
+file = open("key.txt")
+for line in file:
+    apiKey = line
+file.close()
+
+apiRequest += (searchWord + "?key=" + apiKey)
+
+r = requests.get(apiRequest)
+print(r.text)
