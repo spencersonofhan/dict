ebc6b9a79023f2499f1b487db423674a7c947cfa
Spencer Hanson <spencer.hanson@aggiemail.usu.edu>
2020-05-03 22:35:49 -0600

    Added error handling for connection errors and word does not exist/typo errors

--- dict.py
+++ dict.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+import pdb
 
 apiRequest = 'https://www.dictionaryapi.com/api/v3/references/collegiate/json/'
 
@@ -11,11 +12,19 @@ file.close()
 
 apiRequest += (searchWord + "?key=" + apiKey)
 
-req = requests.get(apiRequest)
+try:    
+    req = requests.get(apiRequest)
+except requests.exceptions.RequestException as e: # Check if json was succesfully retrieved
+    print("Something went wrong, attempt to fix connection and try again")
+    sys.exit(0)
+
 jsonData = req.json()
 
-shortDef = jsonData[0]['shortdef'][0]
-descriptor = jsonData[0]['fl']
-
+try:
+    shortDef = jsonData[0]['shortdef'][0]
+    descriptor = jsonData[0]['fl']
+except TypeError as e:
+    print("Word does not exist | typo")
+    sys.exit(0)
 
 print('<' + searchWord + ' (' + descriptor + ')>\n'+ shortDef)
